6945801071888731ecadaaef45b5aecd2e02aef2
Cyrus E <devcyruse@gmail.com>
2019-07-17 10:23:54 -0400

    July 17 part 3 of app is complete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,11 @@
 import React from "react"
 
-function TodoItem() {
+function TodoItem(props) {
+    console.log(props)
     return (
         <div className="todo-item">
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+            <input type="checkbox" checked={props.todo.completed}/>
+            <p>{props.todo.text}</p>
         </div>
     )
 }
